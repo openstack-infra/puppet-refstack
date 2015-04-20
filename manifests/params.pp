bdb9da936f762b50ead05e086cf63444fbf2c6c9
Michael Krotscheck <krotscheck@gmail.com>
2015-04-20 16:45:10 -0700

    Params module
    Centralized configuration module for refstack. All others should pull
    their values from here.

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,7 +4,7 @@
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -12,15 +12,14 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-# == Class: refstack
+# == Class: refstack::params
 #
-# This class installs and updates refstack in a continuous-deployment fashion
-# directly from its git repositories.
+# Centralized configuration management for the refstack module.
 #
-class refstack () {
+class refstack::params () {
 
-  # Configure the entire refstack instance. This does not install anything,
-  # but ensures that variables are consistent across all modules.
-  class { '::refstack::params':
+  # Resolve a few parameters based on the install environment.
+  if $::operatingsystem != 'Ubuntu' or $::operatingsystemrelease < 13.10 {
+    fail("${::operatingsystem} ${::operatingsystemrelease} is not supported.")
   }
 }
