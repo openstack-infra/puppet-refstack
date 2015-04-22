993150a336691a3ce59c389231bbe1ea8dc5ec7c
Michael Krotscheck <krotscheck@gmail.com>
2015-04-22 13:19:18 -0700

    Added charset to mysql recipe.

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -35,5 +35,6 @@ class refstack::mysql () {
     password => $mysql_user_password,
     host     => 'localhost',
     grant    => ['all'],
+    charset => 'latin1', #TODO(krotscheck):Remove once 176364 merges
   }
 }
