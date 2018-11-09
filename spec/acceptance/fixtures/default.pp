595a97c1bb4e22f7794bda53c0baea4e99fd5ccc
Colleen Murphy <colleen@gazlene.net>
2018-11-09 13:34:38 +0100

    [WIP] Add beaker tests
    Add the plumbing to support beaker tests. This is not expected to pass
    yet.
    
    Change-Id: Iaba71391993beade9653d26d47a5d4db321228af

---
+++ spec/acceptance/fixtures/default.pp
@@ -0,0 +1,7 @@
+class { 'refstack':
+  mysql_host          => 'localhost',
+  mysql_database      => 'refstack',
+  mysql_user          => 'refstack',
+  mysql_user_password => 'refstack',
+  protocol            => 'http',
+}
